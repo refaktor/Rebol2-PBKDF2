3d0fcd60b0ce2467a73806ceeaed381831e7dcc8
Janko (HK) <janko@refaktor.net>
2013-03-03 11:15:56 +0100

    some fixes

--- pbkdf2.r
+++ pbkdf2.r
@@ -3,7 +3,6 @@ REBOL []
 pbkdf2: context [
 
  unsigned-to-binary: func [n [number!] /rev][
-  ;THANKS TO http://www.codeconscious.com/rebol/tips-and-techniques.html
   if n > (2 ** 31 - 1) [n: n - (2 ** 32)]
   n: load join "#{" [form to-hex to-integer n "}"]
   either rev [head reverse n][n]
@@ -15,13 +14,9 @@ pbkdf2: context [
   block-cnt: round/ceiling (key-len / hash-len)
   output: copy #{}
   repeat i block-cnt [
-;   print i
    last: join salt unsigned-to-binary i
-;   probe last
    last: xorsum: checksum/key last pwd
-;   probe last
    repeat j (count - 1) [
-;    print j
     xorsum: (xorsum xor (last: checksum/key last pwd))
    ]
    output: join output xorsum
